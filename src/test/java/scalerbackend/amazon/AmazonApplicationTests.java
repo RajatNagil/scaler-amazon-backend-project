aa17ed5db1aceccdd5e00d9e60ce3a5f8b6e398c
RajatNagil <47471052+RajatNagil@users.noreply.github.com>
2026-01-14 07:57:00 +0530

    Initial commit

---
+++ src/test/java/scalerbackend/amazon/AmazonApplicationTests.java
@@ -0,0 +1,13 @@
+package scalerbackend.amazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
