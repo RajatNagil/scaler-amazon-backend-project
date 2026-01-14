5e3db3aae5bb9fbc4aa40c645b79bc183b0b8978
RajatNagil <47471052+RajatNagil@users.noreply.github.com>
2026-01-14 15:26:22 +0530

    Add files via upload

---
+++ AuthController.java
@@ -0,0 +1,27 @@
+package com.amazonclone.controllers;
+
+import com.amazonclone.dto.LoginRequest;
+import com.amazonclone.dto.SignupRequest;
+import com.amazonclone.dto.AuthResponse;
+import com.amazonclone.services.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public AuthResponse signup(@RequestBody SignupRequest request) {
+        return authService.signup(request);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody LoginRequest request) {
+        return authService.login(request);
+    }
+}
